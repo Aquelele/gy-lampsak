d1660b9800ab4b776332934a8f1a78309a9cac69
simon-swe <simon.swedenborg@edu.tibble.nu>
2021-02-12 12:35:49 +0100

    avPa.py på rasberry pi

--- Gy_knapp/avPa.py
+++ Gy_knapp/avPa.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as gpio
-import time
+import datetime
+import os
 def turnOn() {
     gpio.setmode(gpio.BCM)
     gpio.setup(18, gpio.OUT)
@@ -11,8 +12,24 @@ def turnOff() {
     gpio.setup(18, gpio.OUT)
     gpio.output(18, gpio.LOW)
 }
-file = open("times.txt", "r")
-times = file.read(8)
-file.close()
-start = times[0,4]
-stop = times[5, 9]
+var start
+var stop
+dt=datetime.datetime.now()
+if (os.stat("times.txt").st_size != 0){
+    file = open("times.txt", "r")
+    times = file.read(10)
+    file.close()
+    startTim = int(times[0,2])
+    startMin = int(times[3,5])
+    start=(startTim*60+startMin)
+    stopTim = int(times[6,8])
+    stopMin = int(times[9,11])
+    stop = (stopTim*60+stopMin)
+}
+currentTime=(int(dt.hour*60)+int(dt.min))
+if (currentTime==start){
+    turnOn()
+}
+if (currentTime==start){
+    turnOff()
+}

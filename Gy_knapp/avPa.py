383a31c95aa7cc7c4b3034c086c69121d95aef37
Axel Rönnberg <axel.ronnberg.25@gmail.com>
2021-03-12 16:31:11 +0100

    fixar simons skit
    precis som det låter

--- Gy_knapp/avPa.py
+++ Gy_knapp/avPa.py
@@ -2,6 +2,7 @@ import RPi.GPIO as gpio
 import datetime
 import os
 
+
 def turnOn():
     gpio.setmode(gpio.BCM)
     gpio.setup(24, gpio.out)
@@ -12,26 +13,25 @@ def turnOff():
     gpio.setup(24, gpio.out)
     gpio.output(24, True)
 
+
 start=0
 stop=0
 dt=datetime.datetime.now()
-if (os.stat("times.txt").st_size != 0):
-    file = open("times.txt", "r")
-    times0 = file.read(10)
-    file.close()
-    times = []
-    for i in times0:
-        times.append(int(i))
-    startTim = int(times[0,2])
-    startMin = int(times[3,5])
-    start=(startTim*60+startMin)
-    stopTim = int(times[6,8])
-    stopMin = int(times[9,11])
-    stop = (stopTim*60+stopMin)
+file = open("test.txt", "r")
+times = file.readline()
+file.close()
 
-currentTime=(int(dt.hour*60)+int(dt.min))
+startTim = int(times[0] + times[1])
+startMin = int(times[2] + times[3])
+start = startTim*60+startMin
+stopTim = int(times[4] + times[5])
+stopMin = int(times[6] + times[7])
+stop = stopTim*60+stopMin
+
+currentTime= int(dt.hour*60+dt.minute)
+
 if (currentTime==start):
     turnOn()
 
-if (currentTime==start):
+if (currentTime==stop):
     turnOff()

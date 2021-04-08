22446aa5b5bbfaba776deda8652c925328272f42
Axel Rönnberg <axel.ronnberg.25@gmail.com>
2021-04-08 12:46:46 +0200

    typo

--- Gy_knapp/checker.py
+++ Gy_knapp/checker.py
@@ -1,5 +1,5 @@
 while True: #kör altid
-    import time #importerar time så att vi kan använda sime.sleep
+    import time #importerar time så att vi kan använda time.sleep
     import os #importerar os så att vi kan använda os.system
     os.system('sudo python3 /var/www/html/avPa.py') #kör avPå.py 
     time.sleep(60) #väntar 60 sek
